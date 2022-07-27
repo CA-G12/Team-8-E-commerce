fb6311d4873d434e19c8d6feb143eee22cf04de6
Mustafa Salem <mustafaissasalem@gmail.com>
2022-07-27 12:30:51 +0300

    Setting the productsObject to take the array from local storage, added a function which is responsible for adding to local storage, call the function, and add a function to delete a product close #21

--- Seller/seller.js
+++ Seller/seller.js
@@ -1,5 +1,16 @@
 // ? Creating the products array of objects.
-let productsObject = [];
+let productsObjects = JSON.parse(localStorage.getItem("productObject")) || [];
+
+// ? Creating the function which is responsible for adding to local storage.
+function addToLocalStorage(key, source) {
+  window.localStorage.setItem(key, JSON.stringify(source));
+}
 
 // ? Adding the product array of objects to local storage.
-localStorage.setItem("productObject", JSON.stringify(productsObject));
+addToLocalStorage("productObject", productsObjects);
+
+// ? Create the functions which is responsible for deleting a product.
+function deleteProduct(arr, id) {
+  const newArray = deleteFromArray(arr, id);
+  addToLocalStorage("productObject", newArray);
+}
