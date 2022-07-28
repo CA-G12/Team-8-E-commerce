b5f1c691fa98bf5d6f71647fe7914c746fbf4c69
Zayan-Alaraishy <zayan.hassan@gmail.com>
2022-07-28 10:39:37 +0300

    taking data from local storage, add to cart, sorting by price function Relates #14 #15

--- Customer/customer.js
+++ Customer/customer.js
@@ -1,54 +1,56 @@
-let products_section = document.querySelector(".products");
 
-let productsObject = [
-  { id: 1, name: "Coffe" },
-  { id: 2, name: "Tea" },
-];
-let id = 1; 
-const displayAllProducts = () => {
-  console.log("in dislpay");
-  productsObject.forEach((object) => {
+let productsSection = document.querySelector(".products");
+let productsObjects = localStorage.getItem("productObject");
+const displayAllProducts = (arrayOfProducts) => {
+  console.log(arrayOfProducts); 
+  JSON.parse(arrayOfProducts).forEach((object) => {
     console.log(object);
     let newProduct =
-      `<section class="product" data-id="${id++}">
+      `<section class="product" data-id="${object.id}">
         <h2 class="name">` +
       object.name +
       `</h2>
         <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq85Yeu8djaAGvsk1rxT4DNxBHBFEkTCr6FQ&usqp=CAU"
+          src="${object.imageLink}"
           alt="Americano"
           class="image"
         />
-        <p class="price">Price: $<span>5</span></p>
+        <p class="price">Price: $<span>${object.price}</span></p>
         <p class="details">
-          Caffè Americano is a type of coffee drink prepared by diluting an
-          espresso with hot water, giving it a similar strength to, but
-          different flavor from, traditionally brewed coffee
+         ${object.imageLink}
         </p>
         <div class="in-line">
-          <p class="category">Category: Americano</p>
+          <p class="category">Category: ${object.category}</p>
           <button class = "add-to-cart"><i class="fa-solid fa-cart-shopping fa-2xl"></i></button>
         </div>
       </section>`;
-      console.log(newProduct); 
-    products_section.innerHTML = newProduct + products_section.innerHTML;
+    console.log(newProduct);
+    productsSection.innerHTML = newProduct + productsSection.innerHTML;
   });
-};
+}
+//make array with the products in the cart
 let inCart = [];
 localStorage.setItem("in-cart", JSON.stringify(inCart));
 addEventListener("DOMContentLoaded", function () {
-  displayAllProducts();
+  displayAllProducts(productsObjects);
   let addBtn = document.querySelectorAll(".add-to-cart");
   console.log(addBtn);
   addBtn.forEach((button) => {
     button.addEventListener("click", function (e) {
-
-      console.log( e.target.parentElement.parentElement.parentElement.children[2].children[0].textContent); 
+      console.log(
+        e.target.parentElement.parentElement.parentElement.children[2]
+          .children[0].textContent
+      );
       let item = {
-        name: e.target.parentElement.parentElement.parentElement.children[0].textContent,
+        id: e.target.parentElement.parentElement.parentElement.dataset.id,
+        name: e.target.parentElement.parentElement.parentElement.children[0]
+          .textContent,
+        price:
+          e.target.parentElement.parentElement.parentElement.children[2]
+            .children[0].textContent,
+        details: e.target.parentElement.parentElement.parentElement.children[3],
+        category: e.target.parentElement.children[0].children[0],
         amount: 1,
-        price: e.target.parentElement.parentElement.parentElement.children[2].children[0].textContent,
-        id: e.target.parentElement.parentElement.parentElement.dataset.id, 
       };
       if (JSON.parse(localStorage.getItem("in-cart")) === null) {
         inCart.push(item);
@@ -56,9 +58,9 @@ addEventListener("DOMContentLoaded", function () {
       } else {
         const localItems = JSON.parse(localStorage.getItem("in-cart"));
         localItems.forEach((data) => {
-          console.log(item.id , data)
+          console.log(item.id, data);
           if (item.id == data.id) {
-            item.amount = data.amount +1;
+            item.amount = data.amount + 1;
           } else {
             inCart.push(data);
           }
@@ -66,9 +68,15 @@ addEventListener("DOMContentLoaded", function () {
         inCart.push(item);
         localStorage.setItem("in-cart", JSON.stringify(inCart));
       }
-  
-      localStorage.setItem("in-cart", JSON.stringify(addToCart(inCart, item)));
 
+      localStorage.setItem("in-cart", JSON.stringify(addToCart(inCart, item)));
     });
   });
 });
+//Calcualte the total price and add it to local storage
+localStorage.setItem("total-price", JSON.stringify(totalPrice(inCart)));
+//sorting by price
+// let sortByPriceBtn = document.querySelector('#display-types')
+// console.log(sortByPriceBtn.value)
+
+
--- __tests__/pure-functions.test.js
+++ __tests__/pure-functions.test.js
@@ -1,24 +1,39 @@
-const {addToCart, totalPrice} = require('../Customer/logic.js')
+const { addToCart, totalPrice, sortingByPrice } = require("../Customer/logic.js");
 
 test("adding to cart", function () {
   expect(
-    addToCart([
-      { id: 1, name: "Coffee1" },
-      { id: 2, name: "Coffee2" }
-    ], {  id: 3, name: "Coffee3"})
+    addToCart(
+      [
+        { id: 1, name: "Coffee1" },
+        { id: 2, name: "Coffee2" },
+      ],
+      { id: 3, name: "Coffee3" }
+    )
   ).toStrictEqual([
     { id: 1, name: "Coffee1" },
     { id: 2, name: "Coffee2" },
-  {  id: 3, name: "Coffee3"}]);
-}); 
+    { id: 3, name: "Coffee3" },
+  ]);
+});
 test("calc total price", function () {
   expect(
     totalPrice([
-      { id: 1, name: "Coffee1" , price: 5},
-      { id: 2, name: "Coffee2" , price: 3}
+      { id: 1, name: "Coffee1", price: 5 },
+      { id: 2, name: "Coffee2", price: 3 },
     ])
-  ).toStrictEqual(11);
-}); 
+  ).toStrictEqual(8);
+});
+test("sort by price", function () {
+  expect(
+    sortingByPrice([
+      { id: 1, name: "Coffee1", price: 5 },
+      { id: 2, name: "Coffee2", price: 3 },
+    ])
+  ).toStrictEqual([
+    { id: 2, name: "Coffee2", price: 3 },
+    { id: 1, name: "Coffee1", price: 5 },
+  ]);
+});
 const deleteFromArray = require("../Seller/logic");
 
 describe("Testing the function which is responsible for deleting from an array in Seller/logic.js", () => {
@@ -33,4 +48,4 @@ describe("Testing the function which is responsible for deleting from an array i
     const expected = [{ id: 1, name: "Nader" }];
     expect(actual).toStrictEqual(expected);
   });
-  });
+});
